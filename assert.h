13311d30046f788dcb8897cbb6bba9f9f43d3a4b
Shrikrishna-Bhat <118532304+Shrikrishna-Bhat@users.noreply.github.com>
2022-11-18 19:42:59 +0530

    Update assert.h

--- assert.h
+++ assert.h
@@ -21,4 +21,4 @@ typedef struct {
 extern void ColorPairToString(const ColorPair* colorPair, char* buffer);
 extern ColorPair GetColorFromPairNumber(int pairNumber);
 extern int GetPairNumberFromColor(const ColorPair* colorPair);
-//extern void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
+extern void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
