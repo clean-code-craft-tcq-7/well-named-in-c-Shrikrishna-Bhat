3790ad5bace3c738cbdd021ba93fc225dd9ab697
Shrikrishna-Bhat <118532304+Shrikrishna-Bhat@users.noreply.github.com>
2022-11-21 16:06:10 +0530

    Update colorpair.h

--- colorpair.h
+++ colorpair.h
@@ -1,12 +1,15 @@
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
+//const int MAX_COLORPAIR_NAME_CHARS = 16;
 typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
 } ColorPair;
 
 extern int numberOfMinorColors;
+extern const char* MajorColorNames[];
+extern const char* MinorColorNames[];
 //extern void ColorPairToString(const ColorPair* colorPair, char* buffer);
 //extern ColorPair GetColorFromPairNumber(int pairNumber);
 extern int GetPairNumberFromColor(const ColorPair* colorPair);
