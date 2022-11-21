890263db1fe3d8b90e846bf5357a627b03da0f4a
Shrikrishna-Bhat <118532304+Shrikrishna-Bhat@users.noreply.github.com>
2022-11-21 17:02:55 +0530

    Update colorpair.h

--- colorpair.h
+++ colorpair.h
@@ -15,3 +15,4 @@ extern const char* MinorColorNames[];
 extern int GetPairNumberFromColor(const ColorPair* colorPair);
 //extern void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
 extern void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
+extern void printColorPair(enum MajorColor major, enum MinorColor minor);
