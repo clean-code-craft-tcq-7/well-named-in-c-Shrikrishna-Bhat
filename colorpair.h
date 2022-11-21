a032968d5cc2dc00b99986f20b2c2cb589a6933a
Shrikrishna-Bhat <118532304+Shrikrishna-Bhat@users.noreply.github.com>
2022-11-21 12:28:53 +0530

    Update colorpair.h

--- colorpair.h
+++ colorpair.h
@@ -3,8 +3,7 @@ enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
-int numberOfMajorColors =
-    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+
 const char* MinorColorNames[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
