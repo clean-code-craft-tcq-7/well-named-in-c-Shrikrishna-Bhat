0fd67d087e6c434e7e49ee7b9fd2cb4cdd2b387f
Shrikrishna-Bhat <118532304+Shrikrishna-Bhat@users.noreply.github.com>
2022-11-21 17:09:13 +0530

    Update colorpair.c

--- colorpair.c
+++ colorpair.c
@@ -4,8 +4,7 @@
 #include "colorpair.h"
 
 int GetPairNumberFromColor(const ColorPair* colorPair) {
-    return colorPair->majorColor * numberOfMinorColors +
-            colorPair->minorColor + 1;
+    return colorPair->majorColor * numberOfMinorColors + colorPair->minorColor + 1;
 }
 
 void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber)
@@ -16,4 +15,19 @@ void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expected
     int pairNumber = GetPairNumberFromColor(&colorPair);
     printf("Got pair number %d\n", pairNumber);
     assert(pairNumber == expectedPairNumber);
+}
+
+void printColorPair(enum MajorColor major, enum MinorColor minor)
+{
+    int i,j,k;
+    for(i=0;i<25;i++)
+    {
+        for(j=0;i<sizeof(MajorColorNames);i++)
+        {
+            for(k=0;j<sizeof(MinorColorNames);j++)
+            {
+                printf("%d","%d","%d",i+1,);
+            }
+        }
+    }
 }
