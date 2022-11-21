64d48670610fba37be3a3a5283ae714e7360af3e
Shrikrishna-Bhat <118532304+Shrikrishna-Bhat@users.noreply.github.com>
2022-11-21 11:27:29 +0530

    Update colorpair.h

--- colorpair.h
+++ colorpair.h
@@ -21,3 +21,4 @@ extern void ColorPairToString(const ColorPair* colorPair, char* buffer);
 extern ColorPair GetColorFromPairNumber(int pairNumber);
 extern int GetPairNumberFromColor(const ColorPair* colorPair);
 extern void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
+extern void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
