71b58267f80bab9ce467a8c2d39e9a15284cfb1d
Shrikrishna-Bhat <118532304+Shrikrishna-Bhat@users.noreply.github.com>
2022-11-22 15:47:19 +0530

    Update colorpair.c

--- colorpair.c
+++ colorpair.c
@@ -25,10 +25,10 @@ void printColorPair(enum MajorColor major, enum MinorColor minor)
         printf("%d ",i);
         for(j=0;i<numberOfMinorColors;i++)
         {
-            printf("%d ",j);
+            //printf("%d ",j);
             for(k=0;j<numberOfMinorColors;j++)
             {
-                printf("%d ",k);
+                //printf("%d ",k);
             }
         }
     }
