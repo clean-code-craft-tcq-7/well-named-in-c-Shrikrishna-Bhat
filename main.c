ed0fe24e986b1b9f03bfe7bbe61cb3b414b28a8e
Shrikrishna-Bhat <118532304+Shrikrishna-Bhat@users.noreply.github.com>
2022-11-23 17:56:38 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,11 +2,6 @@
 #include <assert.h>
 
 #include "colorpair.h"
-const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
-const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-
-int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
 int main() {
     ColorPair colorPair;
