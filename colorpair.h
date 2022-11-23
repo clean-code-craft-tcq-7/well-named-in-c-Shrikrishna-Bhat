25e0df2c304c81e228a3a3335a9321c373a1463c
Shrikrishna-Bhat <118532304+Shrikrishna-Bhat@users.noreply.github.com>
2022-11-23 17:50:43 +0530

    Update colorpair.h

--- colorpair.h
+++ colorpair.h
@@ -1,7 +1,7 @@
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
-//const int MAX_COLORPAIR_NAME_CHARS = 16;
+extern const int MAX_COLORPAIR_NAME_CHARS = 16;
 typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
