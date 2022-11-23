1a829cb4959a867173fa5df1a9fdb59cba1dac2f
Shrikrishna-Bhat <118532304+Shrikrishna-Bhat@users.noreply.github.com>
2022-11-23 18:02:27 +0530

    Update numberpair.c

--- numberpair.c
+++ numberpair.c
@@ -2,10 +2,10 @@
 #include <assert.h>
 
 #include "colorpair.h"
-const int MAX_COLORPAIR_NAME_CHARS = 16;
+
 const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-
+const int MAX_COLORPAIR_NAME_CHARS = 16;
 int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
